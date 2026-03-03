109297770241925f5c46001995613ed1b6db1e5e
Skull <patelkishan9879@gmail.com>
2026-03-03 10:55:37 +0530

    Final

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: `${import.meta.env.VITE_REACT_BASEURL}`
 })
 
 instance.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
-    if(token) {
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`
     }
 
